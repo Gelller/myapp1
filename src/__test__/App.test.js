eda15b595a934ca8e87d72680213e73284da462e
Gelller <geller.ggs@gmail.com>
2021-09-23 10:58:27 +0300

    lesson-10

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-it('App', () => {
-    const app = shallow(<App />);
-    expect(1).toEqual(1);
+test('renders learn react link', () => {
+    render(<App />);
 });
+
 
